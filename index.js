45a3d213bbecdee9f228026cc2791cb79d52a976
Jamond Turner <jamondturner@yahoo.com>
2022-03-27 22:55:44 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,36 +1,67 @@
-function forEach() {
 
+const array1 = ['a','b','c'];
+
+function forEach(element) {array1.forEach(element)
+
+
+console.log(element);
+
+};
+
+
+
+const array2 = [1, 4, 9, 16]
+
+function map(map1) {array2.map
+
+
+  console.log(map1)
 }
 
-function map() {
-
-}
 
 function filter() {
 
+  const words = ['Celtics', 'Ravens', 'LilGiants', 'Volunteers', 'Mountaineers', 'BadnewsBears'];
+
+  const result = words.filter(word => word.length > 6);
+
+  console.log(result);
 }
 
 function every() {
+
+const isBelowThreshold = (currentValue) => currentValue < 40;
+
+const array1= [1,30, 39,29,10,13]
 
 }
 
 function some() {
 
-}
+  const even = (element) => element % 2 === 0;
 
-function majority() {
+  console.log(array.some(even));
 
-}
-
-function once() {
 
 }
 
-function groupBy() {
+// function majority() {
 
-}
 
-function arrayToObject() {
+
+
+
+// }
+
+// function once() {
+
+// }
+
+// function groupBy() {
+
+// }
+
+// function arrayToObject() {
 
 }
 
